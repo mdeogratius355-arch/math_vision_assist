4cd8d18504d130ee90975b3424f97c44faaaf8ff
mdeogratius355-arch <mdeogratius355@gmail.com>
2026-08-17 14:18:41 +0300

    Update app.jsx

--- app.jsx
+++ app.jsx
@@ -1,46 +1,34 @@
- import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home";
-import Upload from "./pages/Upload";
-import Generate from "./pages/Generate";
-import About from "./pages/About";
-
+import Home from "./home";
+import Upload from "./upload";
+import Generate from "./generate";
 
 function App() {
-
   return (
-
     <BrowserRouter>
 
       <Routes>
 
-        <Route 
-          path="/" 
-          element={<Home />} 
+        <Route
+          path="/"
+          element={<Home />}
         />
 
-        <Route 
-          path="/upload" 
-          element={<Upload />} 
+        <Route
+          path="/upload"
+          element={<Upload />}
         />
 
-        <Route 
-          path="/generate" 
-          element={<Generate />} 
-        />
-
-        <Route 
-          path="/about" 
-          element={<About />} 
+        <Route
+          path="/generate"
+          element={<Generate />}
         />
 
       </Routes>
 
     </BrowserRouter>
-
   );
-
 }
 
-
-export default App;  
+export default App;
